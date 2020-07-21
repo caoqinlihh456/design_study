36f71c670c1610f022187977af402ace1b6bb67a
cql <442510893@qq.com>
2020-07-21 18:24:58 +0800

    Initial commit

---
+++ src/main/java/com/dq/design_study/DesignStudyApplication.java
@@ -0,0 +1,13 @@
+package com.dq.design_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignStudyApplication.class, args);
+	}
+
+}
