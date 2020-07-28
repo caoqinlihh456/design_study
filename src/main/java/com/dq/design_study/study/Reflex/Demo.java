d4d3eace06f69d021313aaa93e4e05f0c55af161
cql <442510893@qq.com>
2020-07-28 23:14:04 +0800

    反射

--- src/main/java/com/dq/design_study/study/Reflex/Demo.java
+++ src/main/java/com/dq/design_study/study/Reflex/Demo.java
@@ -6,13 +6,16 @@ import java.lang.reflect.Field;
  * 反射练习
  */
 public class Demo {
-    public static void main(String[] args) throws NoSuchFieldException {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, InstantiationException {
         //通过反射注入 userSerice
         UserController userController = new UserController();
         Class<? extends UserController> clzz = userController.getClass();
         Field userService = clzz.getDeclaredField("userService");
-        Class<?> serviceClzz = userService.getDeclaringClass();
-//        serviceClzz.
+        userService.setAccessible(true);
+        Class<?> type = userService.getType();
+        Object o = type.newInstance();
+        userService.set(userController,o);
+        System.out.println(userController.getUserService());
 
 
     }
--- src/main/java/com/dq/design_study/study/Reflex/UserController.java
+++ src/main/java/com/dq/design_study/study/Reflex/UserController.java
@@ -5,5 +5,11 @@ public class UserController {
 
     private UserService userService;
 
+    public UserService getUserService() {
+        return userService;
+    }
 
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 }
